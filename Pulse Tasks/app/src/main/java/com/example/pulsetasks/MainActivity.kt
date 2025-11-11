c69da02222f3b88a06db4a05100e999cf8b2559f
PabloLuna25 <jolunava@est.utn.ac.cr>
2025-11-11 06:11:10 -0600

    Elementos gráficos

--- Pulse Tasks/app/src/main/java/com/example/pulsetasks/MainActivity.kt
+++ Pulse Tasks/app/src/main/java/com/example/pulsetasks/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.pulsetasks
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val card = findViewById<CardView>(R.id.loginPanel_main)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in_up)
+        card.post {
+            card.visibility = View.VISIBLE
+            card.startAnimation(animation)
+        }
 
 
     }
